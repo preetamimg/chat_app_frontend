c5917aa3dee5017a5f61f866bd82839f5ec373a7
preetamimg <preetam.img@gmail.com>
2025-05-16 17:57:04 +0530

    expand

--- src/components/CallScreen.jsx
+++ src/components/CallScreen.jsx
@@ -1,10 +1,11 @@
-import { PhoneOff } from 'lucide-react'
+import { Maximize, PhoneOff } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import Avatar from './Avatar'
 
 const CallScreen = ({onReject, remoteStream, myStream}) => {
   const [friend, setFriend] = useState({})
+  const [isSm, setIsSm] = useState(true)
 
   useEffect(()=> {
     const friendDetails = JSON.parse(localStorage.getItem("ACTIVE_CHAT_USER"))?.friendDetails
@@ -17,7 +18,7 @@ const CallScreen = ({onReject, remoteStream, myStream}) => {
           <div className="*:!size-full *:!object-cover size-full">
             {
               remoteStream ? 
-                <ReactPlayer playing muted className="*:!size-full" url={remoteStream}/>
+                <ReactPlayer playing muted className="*:!size-full" url={isSm ? remoteStream : myStream}/>
               : <div className="size-full flex items-center justify-center">
                 <div className="flex flex-col gap-3 items-center">
                   <Avatar size={"big"} name={friend?.userName}/>
@@ -29,9 +30,14 @@ const CallScreen = ({onReject, remoteStream, myStream}) => {
           <div className="*:!size-full [&_video]:object-cover [&_video]:rounded-lg [&_video]:overflow-hidden w-25 md:w-[200px] aspect-[11/16] md:aspect-video absolute bg-white p-1.5 shadow-2xl bottom-3 right-3 z-50 rounded-2xl overflow-hidden">
             {
               myStream ? 
-                <ReactPlayer playing muted className="*:!size-full" url={myStream}/>
+                <ReactPlayer playing muted className="*:!size-full" url={isSm ? myStream : remoteStream}/>
               : "my stream"
             }
+            <div className="absolute top-0 left-0 z-50 size-full bg-black/50 flex items-center justify-center text-white">
+              <button className='cursor-pointer' onClick={()=> setIsSm(!isSm)}>
+                <Maximize size={"20"} />
+              </button>
+            </div>
           </div>
           <button onClick={onReject} className='absolute bottom-0 left-1/2 -translate-y-1/2 size-10 bg-red-500 text-white rounded-full flex items-center justify-center cursor-pointer'><PhoneOff size={16} /></button>
         </div>
